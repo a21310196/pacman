c9d8913d625890306eec972ebf14b3ca22604857
a21310196 <160084874+a21310196@users.noreply.github.com>
2024-11-02 15:45:48 +0000

    Version 4

--- include/TazoDorado.hpp
+++ include/TazoDorado.hpp
@@ -6,10 +6,14 @@ public:
     TazoDorado() {
         this->flotando = false;
     }
+    void Inicializar() {
+        this->flotando = false;
+    }
     ~TazoDorado() {}
     void Flotar(){
         this->flotando = true;
     }
+
     bool Flotando(){
         return this->flotando;
     }
--- makefile
+++ makefile
@@ -1,12 +1,13 @@
 run : bin/tazo
 	./bin/tazo
 
-runMem : bin/mem
+runmem : bin/mem
 	./bin/mem
 bin/tazo : src/Main.cpp
 	g++ src/Main.cpp -Iinclude -o bin/tazo
 
 bin/mem : src/memoria.cpp
 	g++ src/memoria.cpp -Iinclude -o bin/mem
+
 assets/mensaje : bin/tazo
 	./bin/tazo > assets/mensaje
